e2aa1f418dbd85e8d2ed4ec34d745265eb484e70
Colin Stone <colin@bunnylaundering.com>
2014-01-25 13:17:17 -0800

    reading keys

--- src/kiloboltgame/StartingClass.java
+++ src/kiloboltgame/StartingClass.java
@@ -3,14 +3,17 @@ package kiloboltgame;
 import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Frame;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class StartingClass extends Applet implements Runnable {
+public class StartingClass extends Applet implements Runnable, KeyListener {
     @Override
     public void init() {
         // TODO Auto-generated method stub
         setSize(800, 480);
         setBackground(Color.BLACK);
         setFocusable(true);
+        addKeyListener(this);
 
         Frame frame = (Frame) this.getParent().getParent();
         frame.setTitle("Q-Bot Alpha");
@@ -49,5 +52,62 @@ public class StartingClass extends Applet implements Runnable {
             }
         }
 
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+        switch (e.getKeyCode()) {
+        case KeyEvent.VK_UP:
+            System.out.println("Pressed UP");
+            break;
+        case KeyEvent.VK_DOWN:
+            System.out.println("Pressed DOWN");
+            break;
+        case KeyEvent.VK_LEFT:
+            System.out.println("Pressed LEFT");
+            break;
+        case KeyEvent.VK_RIGHT:
+            System.out.println("Pressed RIGHT");
+            break;
+        case KeyEvent.VK_SPACE:
+            System.out.println("Pressed SPACE");
+            break;
+
+        default:
+            break;
+        }
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+        switch (e.getKeyCode()) {
+        case KeyEvent.VK_UP:
+            System.out.println("Released UP");
+            break;
+        case KeyEvent.VK_DOWN:
+            System.out.println("Released DOWN");
+            break;
+        case KeyEvent.VK_LEFT:
+            System.out.println("Released LEFT");
+            break;
+        case KeyEvent.VK_RIGHT:
+            System.out.println("Released RIGHT");
+            break;
+        case KeyEvent.VK_SPACE:
+            System.out.println("Released SPACE");
+            break;
+
+        default:
+            break;
+        }
     }
 }
