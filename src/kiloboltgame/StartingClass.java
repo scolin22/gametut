46907f4242f1bc8de741037ed2c109257b0e38c6
Colin Stone <colin@bunnylaundering.com>
2014-01-25 14:54:51 -0800

    movement done

--- src/kiloboltgame/StartingClass.java
+++ src/kiloboltgame/StartingClass.java
@@ -3,10 +3,18 @@ package kiloboltgame;
 import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.net.URL;
 
 public class StartingClass extends Applet implements Runnable, KeyListener {
+    private Robot robot;
+    private Image image, character;
+    private Graphics second;
+    private URL base;
+
     @Override
     public void init() {
         // TODO Auto-generated method stub
@@ -17,10 +25,19 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
 
         Frame frame = (Frame) this.getParent().getParent();
         frame.setTitle("Q-Bot Alpha");
+        try {
+            base = getDocumentBase();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        // Image setups
+        character = getImage(base, "data/character.png");
     }
 
     @Override
     public void start() {
+        robot = new Robot();
         // TODO Auto-generated method stub
         Thread thread = new Thread(this) {
         };
@@ -44,6 +61,7 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
     public void run() {
         // TODO Auto-generated method stub
         while (true) {
+            robot.update();
             repaint();
             try {
                 Thread.sleep(17);
@@ -51,7 +69,28 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
                 e.printStackTrace();
             }
         }
+    }
 
+    @Override
+    public void update(Graphics g) {
+        // TODO Auto-generated method stub
+        if (image == null) {
+            image = createImage(this.getWidth(), this.getHeight());
+            second = image.getGraphics();
+        }
+        second.setColor(getBackground());
+        second.fillRect(0, 0, getWidth(), getHeight());
+        second.setColor(getForeground());
+        paint(second);
+
+        g.drawImage(image, 0, 0, this);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        // TODO Auto-generated method stub
+        g.drawImage(character, robot.getCenterX() - 61,
+                robot.getCenterY() - 63, this);
     }
 
     @Override
@@ -72,12 +111,15 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
             break;
         case KeyEvent.VK_LEFT:
             System.out.println("Pressed LEFT");
+            robot.moveLeft();
             break;
         case KeyEvent.VK_RIGHT:
             System.out.println("Pressed RIGHT");
+            robot.moveRight();
             break;
         case KeyEvent.VK_SPACE:
             System.out.println("Pressed SPACE");
+            robot.jump();
             break;
 
         default:
@@ -98,9 +140,11 @@ public class StartingClass extends Applet implements Runnable, KeyListener {
             break;
         case KeyEvent.VK_LEFT:
             System.out.println("Released LEFT");
+            robot.stop();
             break;
         case KeyEvent.VK_RIGHT:
             System.out.println("Released RIGHT");
+            robot.stop();
             break;
         case KeyEvent.VK_SPACE:
             System.out.println("Released SPACE");
